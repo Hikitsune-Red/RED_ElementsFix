c4d246c4b1567b62c6418aff01e27a91b7176e18
Hikitsune-Red <hikitsune.red@gmail.com>
2022-10-26 10:26:40 -0500

    Mitigated compatibility issues
    Forgot to add the old compare sort back in; should now mitigate compatibility issues.

--- RED_ElementsFix.js
+++ RED_ElementsFix.js
@@ -1,5 +1,5 @@
 /*:
- * @plugindesc v1.02 A simple plug-in designed to adjust the sprite draw of Time Elements characters
+ * @plugindesc v1.03 A simple plug-in designed to adjust the sprite draw of Time Elements characters
  * @author Hikitsune-Red 火狐
  *
  * @help
@@ -87,10 +87,8 @@
 			flag = 2;
 		}
 		
-		if (a.z !== b.z) {
-			return a.z - b.z;
-		} else if (flag == 0 && a.y !== b.y) {
-			return a.y - b.y;
+		if (flag == 0) {
+			return oldcompare(a, b);
 		} else if (flag == 1 && a._character.screenY() !== b.y) {
 			return a._character.screenY() - b.y;
 		} else if (flag == 2 && a.y !== b._character.screenY()) {
